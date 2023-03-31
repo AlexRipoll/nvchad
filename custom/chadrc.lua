95f74196027a17a589aea2aabd5a1bbe4b2cea8c
Alex Ripoll <alexripollpiera@gmail.com>
2023-03-31 19:41:41 +0200

    enbabling and updating alpha header

--- custom/chadrc.lua
+++ custom/chadrc.lua
@@ -16,6 +16,29 @@ M.ui = {
 		theme = "default",
 		separator_style = "block",
 	},
+
+	nvdash = {
+		load_on_startup = true,
+
+		header = {
+			"                                                     ",
+			"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+			"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+			"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+			"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+			"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+			"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+			"                                                     ",
+		},
+		buttons = {
+			{ "  Find File", "Spc f f", "Telescope find_files" },
+			{ "  Recent Files", "Spc f o", "Telescope oldfiles" },
+			{ "  Find Word", "Spc f w", "Telescope live_grep" },
+			{ "  Bookmarks", "Spc b m", "Telescope marks" },
+			{ "  Themes", "Spc t h", "Telescope themes" },
+			{ "  Mappings", "Spc c h", "NvCheatsheet" },
+		},
+	},
 }
 
 M.plugins = "custom.plugins"
