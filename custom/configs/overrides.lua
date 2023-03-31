d9330057141d8b0bc1c8b5ecf5322a1da73ad2c1
Alex Ripoll <alexripollpiera@gmail.com>
2023-03-31 10:36:29 +0200

    adding lspsaga and extending custom mappings

--- custom/configs/overrides.lua
+++ custom/configs/overrides.lua
@@ -1,92 +1,170 @@
 local M = {}
 
 M.treesitter = {
-  ensure_installed = {
-    "vim",
-    "lua",
-    "rust",
-    "go",
-    "solidity",
-    "javascript",
-    "typescript",
-    "json",
-    "yaml",
-    "html",
-    "css",
-    "markdown",
-    "markdown_inline",
-    "graphql",
-    "bash",
-    "dockerfile",
-    "help",
-  },
-  indent = {
-    enable = true,
-    -- disable = {
-    --   "python"
-    -- },
-  },
+	ensure_installed = {
+		"vim",
+		"lua",
+		"rust",
+		"go",
+		"solidity",
+		"javascript",
+		"typescript",
+		"json",
+		"yaml",
+		"html",
+		"css",
+		"markdown",
+		"markdown_inline",
+		"graphql",
+		"bash",
+		"dockerfile",
+		"help",
+	},
+	indent = {
+		enable = true,
+		-- disable = {
+		--   "python"
+		-- },
+	},
 }
 
 M.mason = {
-  ui = {
-    icons = {
-      package_installed = "✓",
-      package_pending = "➜",
-      package_uninstalled = "✗",
-    },
-  },
-  ensure_installed = {
-    -- lua stuff
-    "lua-language-server",
-    "stylua",
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
+	ensure_installed = {
+		-- lua stuff
+		"lua-language-server",
+		"stylua",
 
-    -- web dev stuff
-    "css-lsp",
-    "html-lsp",
-    "typescript-language-server",
-    "deno",
-    "prettier",
-    "gopls",
-    "rust-analyzer",
-    "solang",
-    "docker-compose-language-service",
-    "dockerfile-language-server",
-    "yaml-language-server",
-  },
+		-- web dev stuff
+		"css-lsp",
+		"html-lsp",
+		"typescript-language-server",
+		"deno",
+		"prettier",
+		"gopls",
+		"rust-analyzer",
+		"solang",
+		"docker-compose-language-service",
+		"dockerfile-language-server",
+		"yaml-language-server",
+	},
 }
 
 -- git support in nvimtree
 M.nvimtree = {
-  git = {
-    enable = true,
-  },
+	git = {
+		enable = true,
+	},
 
-  renderer = {
-    highlight_git = true,
-    icons = {
-      show = {
-        git = true,
-      },
-    },
-  },
+	view = {
+		side = "left",
+		width = 30,
+	},
+
+	update_focused_file = {
+		update_cwd = true,
+	},
+
+	renderer = {
+		highlight_git = true,
+		icons = {
+			show = {
+				git = true,
+			},
+			glyphs = {
+				default = "",
+				symlink = "",
+				folder = {
+					arrow_open = "",
+					arrow_closed = "",
+					default = "",
+					open = "",
+					empty = "",
+					empty_open = "",
+					symlink = "",
+					symlink_open = "",
+				},
+				git = {
+					unstaged = "",
+					staged = "S",
+					unmerged = "",
+					renamed = "➜",
+					untracked = "U",
+					deleted = "",
+					ignored = "◌",
+				},
+			},
+		},
+	},
+	diagnostics = {
+		enable = true,
+		show_on_dirs = true,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "✗",
+		},
+	},
 }
 
 M.telescope = {
-  defaults = {
-    layout_config = {
-      horizontal = {
-        prompt_position = "bottom",
-      },
-    },
-    mappings = {
-      i = {
-        ["<C-k>"] = require("telescope.actions").move_selection_previous, -- move to prev result
-        ["<C-j>"] = require("telescope.actions").move_selection_next, -- move to next result
-        ["<C-q>"] = require("telescope.actions").send_selected_to_qflist + require("telescope.actions").open_qflist, -- send selected to quickfixlist
-      },
-    },
-    extensions_list = { "themes", "terms", "fzf" },
+	defaults = {
+		layout_config = {
+			horizontal = {
+				prompt_position = "bottom",
+			},
+		},
+		mappings = {
+			i = {
+				["<C-k>"] = require("telescope.actions").move_selection_previous, -- move to prev result
+				["<C-j>"] = require("telescope.actions").move_selection_next, -- move to next result
+				["<C-q>"] = require("telescope.actions").send_selected_to_qflist
+					+ require("telescope.actions").open_qflist, -- send selected to quickfixlist
+			},
+		},
+		extensions_list = { "themes", "terms", "fzf" },
+	},
+}
+
+M.gitsigns = {
+  signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+  numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+  linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+  word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+  watch_gitdir = {
+    interval = 1000,
+    follow_files = true
+  },
+  attach_to_untracked = true,
+  current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+    delay = 1000,
+    ignore_whitespace = false,
+  },
+  current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+  sign_priority = 6,
+  update_debounce = 100,
+  status_formatter = nil, -- Use default
+  max_file_length = 40000, -- Disable if file is longer than this (in lines)
+  preview_config = {
+    -- Options passed to nvim_open_win
+    border = 'single',
+    style = 'minimal',
+    relative = 'cursor',
+    row = 0,
+    col = 1
+  },
+  yadm = {
+    enable = false
   },
 }
 
--- custom/mappings.lua
+++ custom/mappings.lua
@@ -2,121 +2,141 @@
 local M = {}
 
 M.general = {
-  n = {
-    -- ["<leaser>h"] = { ":noh <CR>", "clear highlights" },
-    ["<leader>rrnn"] = { "<cmd> set rnu! <CR>", "toggle relative number" },
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+	n = {
+		-- ["<leaser>h"] = { ":noh <CR>", "clear highlights" },
+		["<leader>rrnn"] = { "<cmd> set rnu! <CR>", "toggle relative number" },
+		[";"] = { ":", "enter command mode", opts = { nowait = true } },
 
-    -- vertical movement
-    ["<C-u>"] = { "<C-u>zz", "sccroll half page up and recenter" },
-    ["<C-d>"] = { "<C-d>zz", "sccroll half page down and recenter" },
-    ["<C-f>"] = { "<C-f>zz", "sccroll a full page up and recenter" },
-    ["<C-b>"] = { "<C-f>zz", "sccroll a full page down recenter" },
+		-- vertical movement
+		["<C-u>"] = { "<C-u>zz", "sccroll half page up and recenter" },
+		["<C-d>"] = { "<C-d>zz", "sccroll half page down and recenter" },
+		["<C-f>"] = { "<C-f>zz", "sccroll a full page up and recenter" },
+		["<C-b>"] = { "<C-f>zz", "sccroll a full page down recenter" },
 
-    -- window management 
-    ["<leader>sv"] = { "<C-w>v", "split window vertically" },
-    ["<leader>sh"] = { "<C-w>s", "split window horizontally" },
-    ["<leader>se"] = { "<C-w>=", "make split windows equal width and height" },
-    ["<leader>sx"] = { ":close<CR>", "close current split window" },
-  },
+		-- window management
+		["<leader>sv"] = { "<C-w>v", "split window vertically" },
+		["<leader>sh"] = { "<C-w>s", "split window horizontally" },
+		["<leader>se"] = { "<C-w>=", "make split windows equal width and height" },
+		["<leader>sx"] = { ":close<CR>", "close current split window" },
+	},
 
-  v = {
-    ["<A-j>"] = { ":m'>1<CR>gv=gv", "move code down" },
-    ["<A-k>"] = { ":m-2<CR>gv=gv", "move code up" },
-  }
+	v = {
+		["<A-j>"] = { ":m'>1<CR>gv=gv", "move code down" },
+		["<A-k>"] = { ":m-2<CR>gv=gv", "move code up" },
+	},
 }
 
 M.lspconfig = {
-  n = {
-    ["<leader>rn"] = {
-      function()
-        require("nvchad_ui.renamer").open()
-      end,
-      "lsp rename",
-    },
-  },
+	n = {
+		-- ["<leader>rn"] = {
+		--   function()
+		--     require("nvchad_ui.renamer").open()
+		--   end,
+		--   "lsp rename",
+		-- },
+
+		["gf"] = { "<cmd>Lspsaga lsp_finder<CR>", "show definition, references" },
+
+		["gd"] = { "<cmd>Lspsaga peek_definition<CR>", "lsp declaration" },
+
+		["<leader>ca"] = { "<cmd>Lspsaga code_action<CR>", "lsp code actions" },
+
+		["<leader>rn"] = { "<cmd>Lspsaga rename<CR>", "lsp rename" },
+
+		["<leader>D"] = { "<cmd>Lspsaga show_line_diagnostics<CR>", "lsp line diagnostics" },
+
+		["<leader>d"] = { "<cmd>Lspsaga show_cursor_diagnostics<CR>", "lsp cursor diagnostics" },
+
+		["[d"] = { "<cmd>Lspsaga diagnostic_jump_prev<CR>", "lsp goto prev" },
+
+		["]d"] = { "<cmd>Lspsaga diagnostic_jump_next<CR>", "lsp goto next" },
+
+		["K"] = { "<cmd>Lspsaga hover_doc<CR>", "lsp hover" },
+
+		["<leader>o"] = { "<cmd>LSoutlineToggle<CR>", "lsp outline on right side" },
+	},
 }
 
 M.nvimtree = {
-  n = {
-    -- toggle
-    ["<leader>e"] = { "<cmd> NvimTreeToggle <CR>", "toggle nvimtree" },
+	n = {
+		-- toggle
+		["<leader>e"] = { "<cmd> NvimTreeToggle <CR>", "toggle nvimtree" },
 
-    -- focus
-    ["<C-n>"] = { "<cmd> NvimTreeFocus <CR>", "focus nvimtree" },
-  },
+		-- focus
+		["<C-n>"] = { "<cmd> NvimTreeFocus <CR>", "focus nvimtree" },
+	},
 }
 
 M.telescope = {
-  n = {
-    -- find
-    ["<leader>fs"] = { "<cmd> Telescope live_grep <CR>", "live grep" },
-    ["<leader>fr"] = { "<cmd> Telescope oldfiles <CR>", "find recent files" },
+	n = {
+		-- find
+		["<leader>fs"] = { "<cmd> Telescope live_grep <CR>", "live grep" },
+		["<leader>fr"] = { "<cmd> Telescope oldfiles <CR>", "find recent files" },
 
-    -- git
-    ["<leader>gs"] = { "<cmd> Telescope git_status <CR>", "git status" },
-    ["<leader>gc"] = { "<cmd> Telescope git_commits<CR>", "git commits" },
-  },
+		-- git
+		["<leader>gs"] = { "<cmd> Telescope git_status <CR>", "git status" },
+		["<leader>gc"] = { "<cmd> Telescope git_commits<CR>", "git commits" },
+	},
 }
 
 M.nvterm = {
-  t = {
-    -- toggle in terminal mode
-    ["<C-t>"] = {
-      function()
-        require("nvterm.terminal").toggle "float"
-      end,
-      "toggle floating term",
-    },
-  },
+	t = {
+		-- toggle in terminal mode
+		["<C-t>"] = {
+			function()
+				require("nvterm.terminal").toggle("float")
+			end,
+			"toggle floating term",
+		},
+	},
 
-  n = {
-    ["<C-t>"] = {
-      function()
-        require("nvterm.terminal").toggle "float"
-      end,
-      "toggle floating term",
-    },
-  },
+	n = {
+		["<C-t>"] = {
+			function()
+				require("nvterm.terminal").toggle("float")
+			end,
+			"toggle floating term",
+		},
+	},
 }
 
 M.gitsigns = {
-  n = {
-    ["<leader>gr"] = {
-      function()
-        require("gitsigns").reset_hunk()
-      end,
-      "Reset hunk",
-    },
+	n = {
+		["<leader>gr"] = {
+			function()
+				require("gitsigns").reset_hunk()
+			end,
+			"Reset hunk",
+		},
 
-    ["<leader>gp"] = {
-      function()
-        require("gitsigns").preview_hunk()
-      end,
-      "Preview hunk",
-    },
+		["<leader>gp"] = {
+			function()
+				require("gitsigns").preview_hunk()
+			end,
+			"Preview hunk",
+		},
 
-    ["<leader>gb"] = {
-      function()
-        package.loaded.gitsigns.blame_line()
-      end,
-      "Blame line",
-    },
+		["<leader>gb"] = {
+			function()
+				package.loaded.gitsigns.blame_line()
+			end,
+			"Blame line",
+		},
 
-    ["<leader>gtd"] = {
-      function()
-        require("gitsigns").toggle_deleted()
-      end,
-      "Toggle deleted",
-    },
+		["<leader>gtd"] = {
+			function()
+				require("gitsigns").toggle_deleted()
+			end,
+			"Toggle deleted",
+		},
 
-    ["<leader>gR"] = {
-      function()
-        require("gitsigns").reset_buffer()
-      end,
-      "Toggle deleted",
-    },
-  },
+		["<leader>gR"] = {
+			function()
+				require("gitsigns").reset_buffer()
+			end,
+			"Toggle deleted",
+		},
+	},
 }
 
 return M
